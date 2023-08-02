1fa915764c2633516bcff79ea53313890d0e221a
Satyam Ghodasara <sghodas@gmail.com>
2023-08-02 11:47:42 -0400

    Implement basic swiping and emailing workflow

--- main.py
+++ main.py
@@ -1,17 +1,64 @@
 import csv
 import time
-from datetime import datetime
+from datetime import datetime, date
+import schedule
+import smtplib
+from email.message import EmailMessage
+import os
+import config
 
-# Open a CSV file
-with open("attendance.csv", "w", encoding="UTF8") as f:
-    writer = csv.writer(f)
+CSV_FILE_NAME = "attendance.csv"
 
-    # First column will be the badge id and second column is the timestamp
-    writer.writerow("Badge ID", "Badge Swipe Time")
-    card_info = input("Swipe your badge: ")
-    print(f"Here's what I got: {card_info}")
-    badge_id = card_info.split("=")[1]
-    ts_str = datetime.now().__str__()
 
-    # Write to the CSV file
-    writer.writerow([badge_id, ts_str])
+def send_email():
+    msg = EmailMessage()
+    with open(CSV_FILE_NAME, mode="r", encoding="UTF8") as f:
+        msg.set_content(f.read())
+        msg["Subject"] = config.email_subject + date.today().strftime(
+            " (%A, %B %d, %Y)"
+        )
+        msg["From"] = config.from_email
+        msg["To"] = ", ".join(config.to_emails)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+        smtp_server.login(config.from_email, config.password)
+        smtp_server.sendmail(config.from_email, config.to_emails, msg.as_string())
+
+    os.remove(CSV_FILE_NAME)
+
+
+# Schedule the email
+schedule.every().monday.at(config.send_time).do(send_email)
+schedule.every().tuesday.at(config.send_time).do(send_email)
+schedule.every().wednesday.at(config.send_time).do(send_email)
+schedule.every().thursday.at(config.send_time).do(send_email)
+schedule.every().friday.at(config.send_time).do(send_email)
+
+while True:
+    now = datetime.now()
+    today_send_time = now.replace(
+        hour=int(config.send_time.split(":")[0]),
+        minute=int(config.send_time.split(":")[1]),
+        second=0,
+        microsecond=0,
+    )
+    today_open_time = now.replace(
+        hour=int(config.open_time.split(":")[0]),
+        minute=int(config.open_time.split(":")[1]),
+        second=0,
+        microsecond=0,
+    )
+
+    if now > today_open_time and now < today_send_time:  # If accepting swipes
+        card_info = input("Swipe your badge: ")
+        print(f"Here's what I got: {card_info}")
+        badge_id = card_info.split("=")[1]
+        ts_str = datetime.now().__str__()
+
+        # Write to the CSV file
+        with open(CSV_FILE_NAME, mode="w", encoding="UTF8") as f:
+            writer = csv.writer(f)
+            writer.writerow([badge_id, ts_str])
+    else:  # If not accepting swipes
+        schedule.run_pending()
+        time.sleep(1800)  # Pause for 30 minutes

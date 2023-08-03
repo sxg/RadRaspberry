789384c9c6cf8de77a802397eebcbbe1a42295ff
Satyam Ghodasara <sghodas@gmail.com>
2023-08-03 08:49:54 -0400

    Explicitly setup the CSV file on a schedule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from timedinput import timedinput
 import time
 from datetime import datetime, date
@@ -8,6 +9,12 @@ from email.message import EmailMessage
 import config
 
 CSV_FILE_NAME = "attendance.csv"
+
+
+def setup_csv_file():
+    with open(CSV_FILE_NAME, mode="w", encoding="UTF8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Badge ID", "Badge Swipe Time"])
 
 
 def send_email():
@@ -24,11 +31,13 @@ def send_email():
         smtp_server.login(config.from_email, config.password)
         smtp_server.sendmail(config.from_email, config.to_emails, msg.as_string())
 
-    # Overwrite the existing CSV file with a new one including headers
-    with open(CSV_FILE_NAME, mode="w", encoding="UTF8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Badge ID", "Badge Swipe Time"])
 
+# Setup the CSV file on schedule
+schedule.every().monday.at(config.open_time).do(setup_csv_file)
+schedule.every().tuesday.at(config.open_time).do(setup_csv_file)
+schedule.every().wednesday.at(config.open_time).do(setup_csv_file)
+schedule.every().thursday.at(config.open_time).do(setup_csv_file)
+schedule.every().friday.at(config.open_time).do(setup_csv_file)
 
 # Schedule the email
 schedule.every().monday.at(config.close_time).do(send_email)
@@ -62,6 +71,10 @@ while True:
         elif card_info != "TIMEOUT":  # If the input didn't time out
             badge_id = card_info.split("=")[1]
             ts_str = datetime.now().__str__()
+
+            # Ensure the CSV file exists
+            if not os.path.exists(CSV_FILE_NAME):
+                setup_csv_file()
 
             # Write to the CSV file
             with open(CSV_FILE_NAME, mode="a", encoding="UTF8") as f:

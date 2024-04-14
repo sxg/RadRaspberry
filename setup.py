3541abf7dfa3abcfc02504285618b0146455964a
Satyam Ghodasara <sghodas@gmail.com>
2024-04-14 12:36:52 -0400

    Add metadata to setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rad_raspberry",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["rad_raspberry=rad_raspberry.main:main"]
+    },
+    author="Satyam Ghodasara",
+    author_email="sghodas@gmail.com",
+    description="A tool to take attendance with a Raspberry Pi.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sxg/rad_raspberry",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

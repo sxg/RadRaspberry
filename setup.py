f5e17f65955ccce1f753dbe5162957b1a805f619
Satyam Ghodasara <sghodas@gmail.com>
2024-04-15 22:59:42 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rad_raspberry",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     license="MIT",
     entry_points={

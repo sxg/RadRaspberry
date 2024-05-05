b6855387b4df124a156556ccdc535768e12f037c
Satyam Ghodasara <sghodas@gmail.com>
2024-05-05 12:26:06 -0400

    Change version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rad_raspberry",
-    version="0.2.4",
+    version="1.0.0",
     packages=find_packages(),
     license="MIT",
     entry_points={

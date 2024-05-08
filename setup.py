0a8ca619a00da3c1929f5c573417fa68db6aa95f
Satyam Ghodasara <sghodas@gmail.com>
2024-05-08 16:00:08 -0400

    Move dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="rad_raspberry",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
+    install_requires=[
+        "pandas==2.0.3",
+        "timedinput==0.1.1",
+        "openpyxl==3.1.2",
+        "supabase==2.4.5",
+        "setuptools==69.5.1",
+    ],
     license="MIT",
     entry_points={
         "console_scripts": ["rad_raspberry=rad_raspberry.main:main"]

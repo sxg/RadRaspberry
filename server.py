09a34fb1a5243c91845799e38227d3ce8a402a4c
Ianto Lin Xi <iantolinxi@gmail.com>
2024-06-27 17:42:47 -0400

    update superficial dates, times, formatting, file names

--- server.py
+++ server.py
@@ -1,17 +1,41 @@
+from pathlib import Path
 import os
 from datetime import datetime, timedelta
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import sqlite3
+from datetime import timezone
+from zoneinfo import ZoneInfo
+import json
 import base64
 import pandas as pd
 import requests
 import uvicorn
+import logging
+
+# Create folders for logs, backups, and config file if needed
+ROOT = Path(os.path.expanduser("~/.local/state/rad_raspberry/"))
+LOG_PATH = ROOT / "log"
+BACKUP_PATH = ROOT / "backup"
+RESIDENTS = ROOT / 'residents.csv'
+SWIPES_DB = ROOT / 'swipes.db'
+os.makedirs(LOG_PATH, exist_ok=True)
+os.makedirs(BACKUP_PATH, exist_ok=True)
+
+LOG_FILE_NAME = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_PATH / LOG_FILE_NAME,
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 app = FastAPI()
 
 # Database setup
-conn = sqlite3.connect('swipes.db', check_same_thread=False)
+conn = sqlite3.connect(SWIPES_DB, check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS swipes
@@ -43,6 +67,7 @@ def send_email(to: str, subject: str, body: str, attachments: list = list()):
         "subject": subject,
         "html": body
     }
+    logging.info(f"sending {json.dumps(payload)}")
 
     if len(attachments):
         files = list()
@@ -62,16 +87,14 @@ def send_email(to: str, subject: str, body: str, attachments: list = list()):
         headers=email_headers,
     )
 
-    print(response)
-    print(response.content)
-
     if response.status_code != 200:
+        logging.error("unable to send email", response.content)
         raise HTTPException(status_code=500, detail="Failed to send email")
 
 def get_residents():
     # Load CSV file
     return pd.read_csv(
-        'badge_data.csv',
+        RESIDENTS,
         dtype={
             "name": str,
             "badge_id": str,
@@ -83,50 +106,71 @@ def get_residents():
 @app.post("/swipe")
 async def swipe(data: SwipeData):
     badge_id = data.badge_id
-    timestamp = datetime.now().isoformat()
-
-    # Record swipe to database
-    cursor.execute("INSERT INTO swipes (badge_id, timestamp) VALUES (?, ?)",
-                   (badge_id, timestamp))
-    conn.commit()
+    now = datetime.now(tz=timezone.utc)
+    pretty_now = now.astimezone(ZoneInfo("America/New_York")).strftime('%Y-%m-%d, %I:%M:%S %p')
+    day = datetime.now().astimezone(ZoneInfo("America/New_York")).strftime('%B %d, %Y')
+    logging.info(f"/swipe: {pretty_now} {badge_id} {now}")
 
     r = get_residents()
 
     # Find email for the badge ID
     user_data = r[r['badge_id'] == badge_id]
     if user_data.empty:
+        logging.error(f"unable to find {badge_id}")
         raise HTTPException(status_code=404, detail="Badge ID not found")
+
+    # Record swipe to database
+    cursor.execute("INSERT INTO swipes (badge_id, timestamp) VALUES (?, ?)",
+                   (badge_id, now))
+    conn.commit()
 
     email = user_data['email'].iloc[0]
     name = user_data['name'].iloc[0]
 
     # Send email
-    subject = "Swipe Confirmation"
-    body = f"Hello {name}, your badge (ID: {badge_id}) was swiped at {timestamp}"
+    subject = f"Attendance Confirmed ({day})"
+    body = f"Hello {name}, your attendance has been recorded: </br> {pretty_now} </br> Penn Radiology"
     send_email(email, subject, body)
 
     return {"message": "Swipe recorded and email sent"}
 
+
 @app.post("/send-summary")
 async def send_summary():
+    now = datetime.now().astimezone(ZoneInfo("America/New_York")).strftime('%Y-%m-%d-%H:%M:%S')
+    day = datetime.now().astimezone(ZoneInfo("America/New_York")).strftime('%B %d, %Y')
+    logging.info(f"/send-summary {now}")
+
     # Get swipes from the last day
     yesterday = (datetime.now() - timedelta(days=1)).isoformat()
 
     # Read the data from the database into a DataFrame
-    swipes_df = pd.read_sql_query("SELECT badge_id, timestamp FROM swipes WHERE timestamp > ?", conn, params=[yesterday])
+    swipes_df = pd.read_sql_query(
+        "SELECT badge_id, timestamp FROM swipes WHERE timestamp > ?",
+        conn,
+        params=[yesterday],
+        parse_dates=["timestamp"],
+        )
 
     r = get_residents()
 
     summary_df = swipes_df.merge(r, on='badge_id', how='left')
 
+    as_ny = summary_df.timestamp.dt.tz_convert(ZoneInfo("America/New_York"))
+    summary_df["swipe_time"] = as_ny.dt.strftime('%Y-%m-%d %I:%M:%S %p')
+    summary_df = summary_df.drop("timestamp", axis=1)
+
     # Save as Excel and CSV
-    summary_df.to_excel('summary.xlsx', index=False)
-    summary_df.to_csv('summary.csv', index=False)
+    summary_df.to_excel(BACKUP_PATH / f'{now}.xlsx', index=False)
+    summary_df.to_csv(BACKUP_PATH / f'{now}.csv', index=False)
 
     # Send email with attachments
-    subject = "Daily Swipe Summary"
+    subject = f"Lecture Attendance ({day})"
     body = "Please find attached the daily swipe summary."
-    attachments = ["summary.xlsx", "summary.csv"]
+    attachments = [
+        BACKUP_PATH / f"{now}.xlsx",
+        BACKUP_PATH / f"{now}.csv",
+    ]
     send_email(SUMMARY_RECIPIENT, subject, body, attachments)
 
     return {"message": "Summary sent successfully"}

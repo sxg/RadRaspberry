25094641d206f1c6edb43a4e015118646352bfe6
Ianto Lin Xi <iantolinxi@gmail.com>
2024-06-30 08:16:32 -0400

    rotate

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -2,7 +2,7 @@
 
 # 0 0 * * 0
 
-LOG_DIR=/home/pennradiology/.local/state/rad_raspberry/log/
+LOG_DIR=/home/pennradiology/.local/state/rad_raspberry/log
 
 rm ${LOG_DIR}/last_week/*.log
 mv ${LOG_DIR}/*.log ${LOG_DIR}/last_week/

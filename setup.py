9bceb9acc1260667a96ad500ff90dd7c73563268
Ianto Lin Xi <iantolinxi@gmail.com>
2024-06-30 03:09:50 -0400

    revert

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rad_raspberry",
-    version="1.0.14",
+    version="1.0.15",
     packages=find_packages(),
     install_requires=[
         "pandas==2.0.3",
